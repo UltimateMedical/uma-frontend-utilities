579244f9d932a5a54d2124caea224ab83b66f928
Austin Herman <austin.j.herman@gmail.com>
2019-10-03 14:33:17 -0400

    support development builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: process.env.NODE_ENV || 'production',
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   output: {
