f1f36a2096cc6abddc9c3f5eb78551ea184187e7
Austin Herman <austin.j.herman@gmail.com>
2019-10-17 14:57:33 -0400

    housekeeping

--- dist/QueryStringParser.d.ts
+++
@@ -1,5 +0,0 @@
-import { QueryStringObject } from './contracts';
-declare class QueryStringParser {
-    static getQueryObject(queryString: string): Array<QueryStringObject>;
-}
-export default QueryStringParser;
