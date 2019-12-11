82fb74866f27fe52a3570c443698715cf5e62edc
Austin Herman <austin.j.herman@gmail.com>
2019-12-11 18:27:41 -0500

    generate documentation

--- src/functions/isFalsy.ts
+++ src/functions/isFalsy.ts
@@ -1,4 +1,11 @@
-export default function(value:any) {
+/**
+ * Determine if a given value is falsy.<br>
+ * i.e `null`, `0`, `false`, `''`, `undefined`, `'undefined'`, `NaN`
+ * 
+ * @param {any} value - The value to be evaluated.
+ * @return {boolean} - `true` if falsy. `false` otherwise.
+ */
+export default function isFalsy(value:any):boolean {
   if(
     value === null      || value === 0           || 
     value === false     || value === ''          ||
