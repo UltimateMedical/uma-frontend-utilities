5bdba37d293f054c9553f240eef2fb311dec4deb
Austin Herman <austin.j.herman@gmail.com>
2019-12-12 15:19:46 -0500

    allow null values to be passed to primary and default properties

--- src/query-string/queryStringParamConfigInterface.ts
+++ src/query-string/queryStringParamConfigInterface.ts
@@ -1,7 +1,7 @@
 export default interface QueryStringParamConfigInterface {
   key:string;
 	override?:RegExp
-	primary?:string|boolean|number
-  default?:string|boolean|number
+	primary?:string|boolean|number|null
+  default?:string|boolean|number|null
   excludeIfFalsy?: { primary?:boolean, default?:boolean }|boolean;
 }
